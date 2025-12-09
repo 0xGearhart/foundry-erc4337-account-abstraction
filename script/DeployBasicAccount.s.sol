acd3a9c47db41d3549b2d47140f4865116507e9d
gearhart <geardev26@gmail.com>
2025-12-08 18:05:41 -0800

    creat scripts

---
+++ script/DeployBasicAccount.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {BasicAccount} from "../src/BasicAccount.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeployBasicAccount is Script {}
