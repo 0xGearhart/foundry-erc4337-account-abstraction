e57f2753fed65f1f3840cc8ecc186ab294e1667d
gearhart <geardev26@gmail.com>
2025-12-08 17:58:58 -0800

    add basic account functionality

--- src/BasicAccount.sol
+++ src/BasicAccount.sol
@@ -7,6 +7,7 @@ import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 
 /**
@@ -16,16 +17,73 @@ import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/
  */
 
 contract BasicAccount is IAccount, Ownable {
-    // set owner for OpenZeppelin Ownable constructor at deployment
-    constructor() Ownable(msg.sender) {}
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
 
-    // entrypoint will call this contract
+    error BasicAccount__NotFromEntryPoint();
+    error BasicAccount__NotFromEntryPointOrOwner();
+    error BasicAccount__ExecutionFailed(bytes result);
+
+    /*//////////////////////////////////////////////////////////////
+                                 STATE
+    //////////////////////////////////////////////////////////////*/
+
+    IEntryPoint private immutable i_entryPoint;
+
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    // ensure only the entrypoint can call a function
+    modifier requireFromEntryPoint() {
+        _requireFromEntryPoint(msg.sender);
+        _;
+    }
+
+    // ensure only entrypoint OR owner can call function
+    // adds versatility so owner can call execute themselves if desired
+    modifier requireFromEntryPointOrOwner() {
+        _requireFromEntryPointOrOwner(msg.sender);
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev set owner for OpenZeppelin Ownable constructor at deployment
+     * @param entryPoint address of the entryPoint contract
+     */
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
+
+    /**
+     * @dev need the ability to receive so this contract can pay for transactions
+     * could also use a paymaster but this basic implementation does not
+     */
+    receive() external payable {}
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Only callable from entry point contract
+     * @param userOp struct containing all necessary info for validation and execution of user operations
+     * @param userOpHash hash of the user operation
+     * @param missingAccountFunds amount of funds to transfer to entry point to cover execution costs
+     * @return validationData 0 if signature is valid, 1 if not
+     */
     function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
     )
         external
+        requireFromEntryPoint
         returns (uint256 validationData)
     {
         // struct PackedUserOperation {
@@ -49,9 +107,35 @@ contract BasicAccount is IAccount, Ownable {
         _payPrefund(missingAccountFunds);
     }
 
-    // A signature will be marked valid as long as it is sent by BasicAccount owner
-    // This is the bare minimum for AA account and not very interesting
-    // The possibilities are endless here - you can implement multisig, social recovery, 2FA, etc
+    /**
+     * @notice Execute user operations after validation
+     * @param destination destination address
+     * @param value value to be sent
+     * @param functionData ABI encoded function data
+     */
+    function execute(
+        address destination,
+        uint256 value,
+        bytes calldata functionData
+    )
+        external
+        requireFromEntryPointOrOwner
+    {
+        (bool success, bytes memory result) = destination.call{value: value}(functionData);
+        if (!success) {
+            revert BasicAccount__ExecutionFailed(result);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev A signature will be marked valid as long as it is sent by BasicAccount owner
+     * This is the bare minimum for AA account
+     * The possibilities are endless here - you can implement multisig, social recovery, 2FA, etc
+     */
     function _validateSignature(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash
@@ -71,12 +155,45 @@ contract BasicAccount is IAccount, Ownable {
         return SIG_VALIDATION_SUCCESS; // 0
     }
 
+    /**
+     * @dev Pay back the entrypoint contract
+     */
     function _payPrefund(uint256 missingAccountFunds) internal {
         if (missingAccountFunds != 0) {
-            // pay back entrypoint contract. Could hard code entrypoint contract address instead of msg.sender but this is fine
+            // could hard code entrypoint contract address instead of msg.sender but this is fine
             (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
-            // entry point is responsible for ensuring transfer success so I will just leave as is to satisfy linter
+            // entry point is responsible for ensuring payment is good so I will just leave as is to satisfy linter
             (success);
         }
+    }
+
+    /**
+     * @dev check if sender is entrypoint
+     */
+    function _requireFromEntryPoint(address sender) internal view {
+        if (sender != address(i_entryPoint)) {
+            revert BasicAccount__NotFromEntryPoint();
+        }
+    }
+
+    /**
+     * @dev check if sender is entrypoint or BasicAccount owner
+     */
+    function _requireFromEntryPointOrOwner(address sender) internal view {
+        if (sender != address(i_entryPoint) && sender != owner()) {
+            revert BasicAccount__NotFromEntryPointOrOwner();
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Get the address of the entrypoint contract
+     * @return address address of the entry point
+     */
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
     }
 }
